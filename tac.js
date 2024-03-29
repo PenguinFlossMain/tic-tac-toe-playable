cab6b839f5070bdd53048821b6bc8efdab3ebebe
Joel Abrahams <PenguinFlossMain@gmail.com>
2024-03-29 12:04:25 -0400

    i got frustrated trying to figure out this part and decided to stop for the day

--- tac.js
+++ tac.js
@@ -1,17 +1,19 @@
 let cells = document.querySelectorAll('.row > div');
 let players = ['Joel', 'Leoj']  //PLAYERS IDENTIFIED
-
-
+let XO = ['x', 'o']
+let counter = 0
 for (i = 0; i < cells.length; i++) { // this will be the main argument
-    addEventListener('click', cellClicked ) // maybe cell clicked (player?)
+    addEventListener('click', cellClicked) // maybe cell clicked (player?)
 }
 
-function cellClicked () { 
-    for (o = 0; 0 < players.length; o++) {
-
-    }
-    event.target.textContent = players.length
+function cellClicked(event) { 
+if (counter % 2 == 0) event.target.textContent = 'X';
+else event.target.textContent='O'
+counter++
 }
+
+
+
 // List of things that need to happen
 // Players need to be identified |CHECK|
 // Click needs to display an X or an O | |
@@ -20,3 +22,4 @@ function cellClicked () {
 // there needs to be an identifier to see if someone has won | |
 // When someone wins there needs to be an alert that stops the game | |
 // There needs to be a way to reset the game | |
+// ODDS AND EVENS BOOLIAN FUCK FUCK FUCK FUCK 
